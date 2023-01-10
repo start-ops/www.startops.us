d03a2152afbc5b58c271cfdfca371b5f3c820e8f
Jim Park <jimp@scribd.com>
2023-01-09 20:55:01 -0800

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.startops.us/',
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx()] // Add renderers to the config
-
+  integrations: [sitemap(), mdx(), react()] // Add renderers to the config
 });
